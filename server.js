db5e3ec93cc1461b2fb7a2515f51bead0a887c4c
Shubham Gupta <gshubham55@gmail.com>
2013-03-07 23:52:37 +0530

    Updated: close connection + error reporting

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-// var sys = require("sys"),  
-// http = require("http"); 
-// var count =0; 
-// http.createServer(function(request,response){  
-//     count++;
-//     sys.puts("it got hit again : "+count);  
-//     //console.log(JSON.stringify(request.headers));
-//     response.writeHeader(200, {"Content-Type": "text/plain"});  
-//     response.write("Hello World!");  
-//     response.end();  
-// }).listen(8080)
-// sys.puts("Server Running on 8080"); 
 var net = require('net');
 var sys = require('sys');
 var host = '127.0.0.1';
 var port = 8080;
 var count =0;
-net.createServer(function (socket) {
-	console.log('connected: ' + socket.remoteAddress);
+var server = net.createServer(function (socket) {
+	
 	
 	count++;
 	socket.addListener("connect", function () {
-    sys.puts("Connection from " + socket.remoteAddress + "count :" + count);
-    socket.write("Hello World\n");
+        console.log('\nconnected: ' + socket.remoteAddress);
+        sys.puts( "count : " + count);
+    
     
     });
 	socket.addListener("data", function (data) {
     socket.write(data);
     socket.end();
     });
-        
-    
+    socket.write("Hello World!\n");
+
+    socket.on("end",function()
+    {
+        console.log("Connection closed");
+    });
+    socket.on("error", function (error)
+    {
+        if (error.code=='EADDRINUSE') {
+            console.log("Address/port in use, retrying....");
+            setTimeout(function(){
+                server.close();
+                server.listen(port,host);
+            },1000);
+        }
+    });
+
 }).listen(port,host);
 console.log("server running on 8080");
